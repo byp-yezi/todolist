da9a4a285a4cf8a5eae0fdfc275902760e730cf7
baoyoupeng <310757801@qq.com>
2024-11-12 15:56:48 +0800

    first

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import conf "todolist/config"
+
+func main() {
+	loading()
+}
+
+func loading() {
+	conf.Init()
+}
---
+++ config/conf.go
@@ -0,0 +1,53 @@
+package conf
+
+import (
+	"log"
+
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode  string
+	HttpPort string
+
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassWord string
+	DbName     string
+
+	RedisDb     string
+	RedisAddr   string
+	RedisPw     string
+	RedisDbName string
+)
+
+func Init() {
+	file, err := ini.Load("config/config.ini")
+	if err != nil {
+		log.Println("config read err", err)
+	}
+	LoadService(file)
+}
+
+func LoadService(file *ini.File) {
+	AppMode = file.Section("service").Key("AppMode").String()
+	HttpPort = file.Section("service").Key("HttpPort").String()
+}
+
+func LoadMysql(file *ini.File) {
+	Db = file.Section("mysql").Key("Db").String()
+	DbHost = file.Section("mysql").Key("DbHost").String()
+	DbPort = file.Section("mysql").Key("DbPort").String()
+	DbUser = file.Section("mysql").Key("DbUser").String()
+	DbPassWord = file.Section("mysql").Key("DbPassWord").String()
+	DbName = file.Section("mysql").Key("DbName").String()
+}
+
+func LoadRedis(file *ini.File) {
+	RedisDb = file.Section("redis").Key("RedisDb").String()
+	RedisAddr = file.Section("redis").Key("RedisAddr").String()
+	RedisPw = file.Section("redis").Key("RedisPw").String()
+	RedisDbName = file.Section("redis").Key("RedisDbName").String()
+}
